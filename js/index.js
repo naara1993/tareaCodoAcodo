ee8db74e57381eda364e955cced2544985d0af01
naara cataldo <naarabelencataldo@outlook.com>
2022-09-21 12:56:36 -0300

    =final

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function abrir(e){
+    e.preventDefault();
+    const navbarSupportedContent=document.getElementById('navbarSupportedContent');
+    navbarSupportedContent.classList.toggle('show')
+}
